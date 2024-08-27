eb4cc252bfdfd42dec302381df6bc892c2d8b5d4
Jacob <104758482+JaFu-py@users.noreply.github.com>
2024-08-27 10:52:46 +0100

    Add typewriter

---
+++ components/typewriter.svelte
@@ -0,0 +1,47 @@
+<!-- ========================================================================
+
+  Typewriter
+
+  Insert description here.
+
+  Dependencies:
+  - Svelte
+  - Nothing else
+
+  Usage:
+  Paste this somewhere and sue as component.
+ 
+-->
+<script lang="ts">
+	const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	let { text, ...props }: { text: string } = $props();
+	let newText = $state(text);
+	function hacker() {
+		let interval: any = null;
+
+		let iteration = 0;
+
+		clearInterval(interval);
+
+		interval = setInterval(() => {
+			newText = newText
+				.split("")
+				.map((letter, index) => {
+					if (index < iteration) {
+						return text[index];
+					}
+
+					return letters[Math.floor(Math.random() * 26)];
+				})
+				.join("");
+
+			if (iteration >= text.length) {
+				clearInterval(interval);
+			}
+
+			iteration += 1 / 5;
+		}, 30);
+	}
+</script>
+
+<span onmouseover={hacker} {...props}>{newText}</span>
